b8faa56e55ddf908522c50905c7e232bbf278c58
Lucien Schreiber <lucien.schreiber@terranum.ch>
2023-11-12 15:49:29 +0100

    Adding code for uploading using pymcuprog

--- Code/PIO/Sensor/src/test_serial.cpp
+++ Code/PIO/Sensor/src/test_serial.cpp
@@ -4,14 +4,14 @@
 #include "Arduino.h"
 #include <SPI.h>
 
-#define PIN_LATCH 7
+#define PIN_LATCH 3
 const int LOOP_NUMBER = 15;
 int LOOP_ACTUAL = 0;
 
 void setup(){
     pinMode(PIN_LATCH, OUTPUT);  // LATCH
     digitalWrite(PIN_LATCH, HIGH);
-    Serial.begin(9600);  // Pin6 RX Pin7 TX
+    Serial.begin(1200);  // Pin6 RX Pin7 TX
     analogReference(VDD);
     delay(5);
     Serial.println("Starting...");
